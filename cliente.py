5213a44ed7072e5ae2ed256d295f04c88b2104e3
marciote0602 <marciote0602@gmail.com>
2022-05-26 16:21:21 -0300

    clone repository

---
+++ cliente.py
@@ -0,0 +1,13 @@
+class Cliente:
+    def __init__(self, nome, sexo, idade):
+        self.nome = nome
+        self.sexo = sexo
+        self.idade = idade
+
+    def comprar(self):
+        print(f'{self.nome} está comprando')
+        
+        
+    
+class Produto:
+    pass
